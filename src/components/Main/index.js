2391ea8ac8394904cb8c730f7ad180914828dc86
Julio Velezmoro <julio28ve@gmail.com>
2022-11-10 16:56:41 +0000

    Task 6

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,15 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './index.css';
+import "./index.css";
 
-import samplePosts from '../../libs/data';
+import samplePosts from "../../libs/data";
 
-import Post from '../Post';
+import Post from "../Post";
 
 function Main() {
   const [posts, setPosts] = useState(samplePosts);
 
-  return <main id="main"></main>;
+  return (
+    <main id="main">
+      <div className="container">
+        {posts.map((post) => (
+          <Post
+            key={post.postId}
+            title={post.title}
+            date={post.date}
+            author={post.author}
+            text={post.text}
+            highlights={post.highlights}
+            image={post.image}
+          />
+        ))}
+      </div>
+    </main>
+  );
 }
 
 export default Main;
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,8 +2,22 @@ import React from 'react';
 
 import './index.css';
 
-function Post() {
-  return <article></article>;
+function Post(prop) {
+  return <article>
+  <h2>{prop.title}</h2>
+  <h5>{prop.date}</h5>
+  <h4>{prop.author}</h4>
+
+  <p>{prop.text}</p>
+  <ul>
+    {prop.highlights.map((highlight) => (
+      <li>{highlight}</li>
+    ))}
+  </ul>
+  
+  <img src={prop.image.link} alt={prop.image.alt} />
+
+  </article>;
 }
 
 export default Post;
