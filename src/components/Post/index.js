168376dfeb7b92ea18191892b961776e04c019df
Julio Velezmoro <julio28ve@gmail.com>
2022-11-10 17:08:18 +0000

    task 6

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -13,24 +13,6 @@ function Post(props) {
     <img src={props.image.link} alt={props.image.alt} />
     </article>
 	);
-import './index.css';
-
-function Post(prop) {
-  return <article>
-  <h2>{prop.title}</h2>
-  <h5>{prop.date}</h5>
-  <h4>{prop.author}</h4>
-
-  <p>{prop.text}</p>
-  <ul>
-    {prop.highlights.map((highlight) => (
-      <li>{highlight}</li>
-    ))}
-  </ul>
-  
-  <img src={prop.image.link} alt={prop.image.alt} />
-
-  </article>;
 }
 
 export default Post;
